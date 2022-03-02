7239d4ba5e1600c1977754c91bcac42a4a4d9918
Sergio Gomez <seluigo@hotmail.com>
2022-03-02 15:45:45 -0500

    Variables, operaciones, tipos de datos I

---
+++ clase_uno_python.py
@@ -0,0 +1,78 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+
+#Variables
+a = 3
+print(type(a))
+a = "Hola mundno"
+print(type(a))
+a = 3.5
+print(type(a))
+a = True
+print(type(a))
+
+print("I can't do it")
+print('His name is "Sergio"')
+
+#Operaciones
+#Suma
+a = 5
+b = 2 
+c = a + b
+print(c)
+
+#Resta
+a = 5
+b = 2 
+c = a - b
+print(c)
+
+#Multipicacion
+a = 5
+b = 2 
+c = a * b
+print(c)
+
+#Divison
+a = 5
+b = 2 
+c = a / b
+print(c)
+
+#Division parte entera
+a = 5
+b = 2 
+c = a//b
+print(c)
+
+#Potencia
+a = 2
+c = a ** 6
+print(c)
+
+#Raiz cuadrada
+a = 16
+b  = a ** (1/2)
+print(b)
+
+#Raiz cuadrada con immport math
+import math 
+a = 16
+raiz = math.sqrt(a)
+print(raiz)
+
+#Raiz cubica
+a = 16
+b  = a ** (1/3)
+print(b)
+
+#Tipos de datos
+#String str
+a = "Hola mundo"
+a = 'Hola mundo'
+b = "I can't do it"
+c = 'Alias "Checho"'
